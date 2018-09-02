988dd59a741118ac379171c30976a14d336ae847
serbanm <serbanmarin@hotmail.ca>
2018-09-01 20:58:25 -0400

    UI mockup

---
+++ app/src/main/java/sherban/whosturn/GameState.java
@@ -0,0 +1,4 @@
+package sherban.whosturn;
+
+public class GameState {
+}
--- app/src/main/java/sherban/whosturn/MainActivity.java
+++ app/src/main/java/sherban/whosturn/MainActivity.java
@@ -1,13 +1,37 @@
 package sherban.whosturn;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout button;
+
+    private TextView playerName;
+
+    private TextView turnTimer;
+
+    private TextView reserveTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        button = (LinearLayout) findViewById(R.id.button7);
+        playerName = (TextView) findViewById(R.id.playerName);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "00:00:00";
+                playerName.setText(message);
+            }
+        });
     }
 }
